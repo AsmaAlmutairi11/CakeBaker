7f3bad39c9ebe8a0691765c5d8c0cf57d5b78b91
Asma <Asmamutairi11@gmail.com>
2024-10-20 16:13:07 +0300

    Initial Commit

---
+++ CakeBaker/CakeBakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeBakerApp.swift
+//  CakeBaker
+//
+//  Created by Asma Mohammed on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct CakeBakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CakeBakerTests/CakeBakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CakeBakerTests.swift
+//  CakeBakerTests
+//
+//  Created by Asma Mohammed on 17/04/1446 AH.
+//
+
+import Testing
+@testable import CakeBaker
+
+struct CakeBakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
